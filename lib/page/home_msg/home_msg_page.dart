32f886b4dcc0b10ff8d31899c4c551826b5e63d1
onetwo <casey@gamil.com>
2023-12-13 21:50:10 +0800

    ui修改

--- lib/page/home_msg/home_msg_page.dart
+++ lib/page/home_msg/home_msg_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/widget/common_widget/ys_pull_refresh.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class HomeMsgPage extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() {
     return _HomeMsgPageState();
@@ -10,11 +11,28 @@ class HomeMsgPage extends StatefulWidget {
 }
 
 class _HomeMsgPageState extends State<HomeMsgPage> {
+  RefreshController controller = RefreshController();
 
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.fromLTRB(16, 12, 16, 0),
+        child: Column(
+          children: [
+            Expanded(child: pullYsRefresh(
+              refreshController: controller,
+              enablePullUp: false,
+              child: ListView.builder(
+                itemCount: 0,
+                itemBuilder: (context, index) {
+                  return Container();
+                },
+              ),
+            ),),
+          ],
+        ),
+      ),
+    );
   }
-
 }
