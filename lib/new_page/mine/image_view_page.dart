0afc197a833ea280112d35acd9fbeeb415e6306f
wanye <wanye@gmail.com>
2023-12-15 23:30:37 +0800

    完成图片查看

--- lib/new_page/mine/image_view_page.dart
+++ lib/new_page/mine/image_view_page.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/common/image/custom_network_image.dart';
+import 'package:flutter_app/common/image/custom_network_image_new.dart';
 import 'package:flutter_app/new_page/mine/floating_cs_view.dart';
 import 'package:flutter_app/widget/appbar/custom_appbar.dart';
 import 'package:flutter_app/widget/full_bg.dart';
+import 'package:flutter_screenutil/screen_util.dart';
 
 ///帮助反馈
 class ImageViewPage extends StatefulWidget {
+  List<String> seriesCover;
+  int realDataIndex=0;
+  ImageViewPage(this.seriesCover,this.realDataIndex);
   @override
   State<StatefulWidget> createState() {
     return _ImageViewPageState();
@@ -12,6 +18,8 @@ class ImageViewPage extends StatefulWidget {
 }
 
 class _ImageViewPageState extends State<ImageViewPage> {
+  List<String> listdata = [""];
+  PageController _pageController = PageController();
 
   @override
   void initState() {
@@ -20,34 +28,56 @@ class _ImageViewPageState extends State<ImageViewPage> {
 
   @override
   Widget build(BuildContext context) {
+    int indexs = 1;
+    int mx = listdata.length;
     return FullBg(
       child: Scaffold(
-          appBar: CustomAppbar(title: ""),
-          body: Stack(
-            children: [
-              PageView(
-                child: Container(
-                  margin: EdgeInsets.symmetric(horizontal: 16),
-                  padding: EdgeInsets.only(bottom: 50),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "常见问题与反馈",
-                        style: const TextStyle(
-                            color: const Color(0xffffffff), fontWeight: FontWeight.w600, fontSize: 24.0),
-                      ),
-                      SizedBox(height: 10),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: questionList.map((e) => _getItem(context, e)).toList(),
-                      ),
-                    ],
-                  ),
+        appBar: CustomAppbar(title: ""),
+        body:
+
+            //定义两个全局变量indexs为当前页数    mx 为最大页数
+            Stack(
+          alignment: const FractionalOffset(0.9, 0.95), //调整位置
+          children: <Widget>[
+            Container(
+              height: ScreenUtil().setHeight(428.0),
+              width: ScreenUtil().setWidth(343.0),
+              child: PageView(
+                onPageChanged: (index) {
+                  //index为当前是第几个
+                  setState(() {
+                    indexs = index + 1;
+                  });
+                },
+                controller: _pageController,
+                children: listdata.map((e) => _image()).toList(), //写图片组件
+              ),
+            ),
+            Positioned(
+              child: Container(
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black45,
+                        offset: Offset(0.0, 0.0), //阴影xy轴偏移量
+                        blurRadius: 1.0, //阴影模糊程度
+                        spreadRadius: 2.0 //阴影扩散程度
+                        )
+                  ],
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                alignment: Alignment.center,
+                width: ScreenUtil().setWidth(50.0),
+                height: ScreenUtil().setHeight(20.0),
+                child: Text(
+                  '$indexs/' + '$mx',
+                  style: TextStyle(color: Colors.white),
                 ),
               ),
-            ],
-          )),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
@@ -57,15 +87,38 @@ class _ImageViewPageState extends State<ImageViewPage> {
         child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
           Text(
             item["question"],
-            style:
-            const TextStyle(color: const Color(0xffc6c6c6), fontWeight: FontWeight.w700, fontSize: 16.0),
+            style: const TextStyle(
+                color: const Color(0xffc6c6c6),
+                fontWeight: FontWeight.w700,
+                fontSize: 16.0),
           ),
           SizedBox(height: 5),
           Text(
             item["answer"],
-            style:
-            const TextStyle(color: const Color(0xff808080), fontWeight: FontWeight.w400, fontSize: 14.0),
+            style: const TextStyle(
+                color: const Color(0xff808080),
+                fontWeight: FontWeight.w400,
+                fontSize: 14.0),
           ),
         ]));
+  }
+
+  _image() {
+    return ClipRRect(
+      borderRadius: BorderRadius.all(Radius.circular(4)),
+      child: Stack(
+        fit: StackFit.expand,
+        alignment: AlignmentDirectional.center,
+        children: [
+          CustomNetworkImageNew(
+            width: double.maxFinite,
+            height: 344,
+            imageUrl: getImagePath(widget.seriesCover[widget.realDataIndex], true, false),
+            fit: BoxFit.cover,
+            useQueue: true,
+          ),
+        ],
+      ),
+    );
   }
 }
